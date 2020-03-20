7f262a4aafd2798b61e047dd1b19c040430f418c
oscarpm5 <47557468+oscarpm5@users.noreply.github.com>
2020-03-20 18:29:53 +0100

    0.1.25.0 Added Event enum

--- Proyecto2/Motor2D/EventManager.h
+++ Proyecto2/Motor2D/EventManager.h
@@ -6,10 +6,19 @@
 #include "vector"
 #include <map>
 
+enum class EVENT_ENUM
+{
+	AUDIO_INCOMES,
+	KEYBOARD_INPUT,
+	MOUSE_INPUT,
+	PAUSE_GAME,
+	NULL_EVENT
+};
+
 class ModuleEventManager : public Module
 {
 public:
-
+	
 	ModuleEventManager();
 	virtual ~ModuleEventManager();
 
@@ -20,7 +29,7 @@ public:
 	bool CleanUp();
 
 private:
-	//	std::map<> eventMap;
+	std::map<EVENT_ENUM,std::vector<Module*>> eventListenersMap;
 };
 
 #endif //__EVENTMANAGER_H__
