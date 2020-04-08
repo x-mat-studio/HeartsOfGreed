105c2acdc8733783b26f66170a08d07c3ae78719
adriaserrano97 <adria.serrano.97@gmail.com>
2020-04-08 18:47:34 +0200

    0.4.32.0. Buildings fully spawn via tiled
    DO NOT MESS WITH TILESET

--- Hearts Of Greed/Motor2D/Map.cpp
+++ Hearts Of Greed/Motor2D/Map.cpp
@@ -460,7 +460,7 @@ bool ModuleMap::LoadLayer(pugi::xml_node& layer_node, MapLayer* layer)
 				colliderRectAux.x = tilePosAux_x * colliderRectAux.w / 2 - tilePosAux_y * colliderRectAux.w / 2;
 				colliderRectAux.y = tilePosAux_x * colliderRectAux.h / 2 + tilePosAux_y * colliderRectAux.h / 2;
 				colliderRectAux.w /= 2;
-
+				
 				app->coll->AddCollider(colliderRectAux, COLLIDER_WALL);
 
 			}
@@ -482,8 +482,39 @@ bool ModuleMap::LoadLayer(pugi::xml_node& layer_node, MapLayer* layer)
 				colliderRectAux.x = tilePosAux_x * colliderRectAux.w / 2 - tilePosAux_y * colliderRectAux.w / 2;
 				colliderRectAux.y = tilePosAux_x * colliderRectAux.h / 2 + tilePosAux_y * colliderRectAux.h / 2;
 				colliderRectAux.w /= 2;
-
-				app->entityManager->AddEntity(ENTITY_TYPE::BLDG_BASE, colliderRectAux.x, colliderRectAux.y);
+				
+				switch (layer->gid[i])
+				{
+					case 390:
+						app->entityManager->AddEntity(ENTITY_TYPE::BLDG_BASE, colliderRectAux.x, colliderRectAux.y,ENTITY_ALIGNEMENT::PLAYER);
+						break;
+					case 391:
+						app->entityManager->AddEntity(ENTITY_TYPE::BLDG_BASE, colliderRectAux.x, colliderRectAux.y, ENTITY_ALIGNEMENT::ENEMY);
+						break;
+					case 392:
+						app->entityManager->AddEntity(ENTITY_TYPE::BLDG_BASE, colliderRectAux.x, colliderRectAux.y);
+						break;
+					case 393:
+						app->entityManager->AddEntity(ENTITY_TYPE::BLDG_UPGRADE_CENTER, colliderRectAux.x, colliderRectAux.y, ENTITY_ALIGNEMENT::PLAYER);
+						break;
+					case 394:
+						app->entityManager->AddEntity(ENTITY_TYPE::BLDG_UPGRADE_CENTER, colliderRectAux.x, colliderRectAux.y, ENTITY_ALIGNEMENT::ENEMY);
+						break;
+					case 395:
+						app->entityManager->AddEntity(ENTITY_TYPE::BLDG_UPGRADE_CENTER, colliderRectAux.x, colliderRectAux.y);
+						break;
+					case 396:
+						app->entityManager->AddEntity(ENTITY_TYPE::BUILDING, colliderRectAux.x, colliderRectAux.y, ENTITY_ALIGNEMENT::PLAYER);
+						break;
+					case 397:
+						app->entityManager->AddEntity(ENTITY_TYPE::BUILDING, colliderRectAux.x, colliderRectAux.y, ENTITY_ALIGNEMENT::ENEMY);
+						break;
+					case 398:
+						app->entityManager->AddEntity(ENTITY_TYPE::BUILDING, colliderRectAux.x, colliderRectAux.y);
+						break;
+					default:
+						break;
+				}
 
 			}
 		}
