4607fed59f3bd95ae509d658c5124dabb51fa86e
Ferran-Roger Basart i Bosch <47557484+ferba93@users.noreply.github.com>
2020-04-10 20:32:10 +0200

    0.5.15.1 Suitman animations checked and corrected

--- Hearts Of Greed/Motor2D/Hero.h
+++ Hearts Of Greed/Motor2D/Hero.h
@@ -57,8 +57,8 @@ class Hero : public DynamicEntity
 public:
 	Hero(fMPoint position, ENTITY_TYPE type, Collider* col,
 		Animation& walkLeft, Animation& walkLeftUp, Animation& walkLeftDown, Animation& walkRightUp,
-		Animation& walkRightDown, Animation& walkRight, Animation& idleRight, Animation& idleRightDown,
-		Animation& idleRightUp, Animation& idleLeft, Animation& idleLeftUp, Animation& idleLeftDown,
+		Animation& walkRightDown, Animation& walkRight, Animation& idleRight, Animation& idleRightUp,
+		Animation& idleRightDown, Animation& idleLeft, Animation& idleLeftUp, Animation& idleLeftDown,
 		int level, int hitPoints, int recoveryHitPointsRate, int energyPoints, int recoveryEnergyRate,
 		int attackDamage, int attackSpeed, int attackRange, int movementSpeed, int vision, float skill1ExecutionTime,
 		float skill2ExecutionTime, float skill3ExecutionTime, float skill1RecoverTime, float skill2RecoverTime, float skill3RecoverTime);
