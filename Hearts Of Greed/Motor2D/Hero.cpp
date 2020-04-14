0ede5535720cd574bcc4003623a1c4efcbde85e2
AlexMelenchon <47743805+AlexMelenchon@users.noreply.github.com>
2020-04-14 13:29:28 +0200

    0.6.2.1 Grenade animation added
    - Needs timing ajustments

--- Hearts Of Greed/Motor2D/Hero.cpp
+++ Hearts Of Greed/Motor2D/Hero.cpp
@@ -599,12 +599,10 @@ void Hero::SkillCanceled()
 }
 
 
-
 void Hero::LevelUp()
 {
 	return;
 }
-
 
 
 int Hero::RecieveDamage(int damage)
@@ -680,16 +678,16 @@ void Hero::InternalInput(std::vector<HERO_INPUTS>& inputs, float dt)
 
 		if (skill1TimePassed >= skill1ExecutionTime)
 		{
-			//inputs.push_back(HERO_INPUTS::IN_SKILL1);
 			
 			inputs.push_back(HERO_INPUTS::IN_SKILL_FINISHED);
-			skill1TimePassed = 0.f;
 
 			if (skillExecutionDelay)
 			{
 				ExecuteSkill1();
 				skillExecutionDelay = false;
 			}
+
+			skill1TimePassed = 0.f;
 		}
 	}
 
