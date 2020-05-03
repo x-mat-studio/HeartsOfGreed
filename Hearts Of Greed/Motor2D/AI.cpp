5909fcad09eec446a826d2fb3d736590cfd00860
AlexMelenchon <47743805+AlexMelenchon@users.noreply.github.com>
2020-05-03 17:03:21 +0200

    0.8.4.4 Base alarm colliders work again

--- Hearts Of Greed/Motor2D/AI.cpp
+++ Hearts Of Greed/Motor2D/AI.cpp
@@ -54,8 +54,8 @@ void ModuleAI::OnCollision(Collider* c1, Collider* c2)
 	{
 		CreateSelectionCollider(c1);
 
-		if(c2->thisEntity)
-		objectivePos = c2->thisEntity->GetPosition();
+		if (c2->thisEntity)
+			objectivePos = c2->thisEntity->GetPosition();
 	}
 }
 
@@ -70,7 +70,7 @@ void ModuleAI::CreateSelectionCollider(Collider* collider)
 
 fMPoint* ModuleAI::GetObjective()
 {
-	if (objectivePos != fMPoint{INT_MIN, INT_MIN })
+	if (objectivePos != fMPoint{ INT_MIN, INT_MIN })
 	{
 		return &objectivePos;
 	}
@@ -92,12 +92,12 @@ void ModuleAI::ExecuteEvent(EVENT_ENUM eventId)
 		if (base != -1) //-1 means no player controlled bases were found
 		{
 			objectivePos = baseVector[base]->GetPosition() + baseVector[base]->GetCenter();
-			
+
 			//call nearest spawner and spawn x number of monsters
 			CommandSpawners();
 		}
 
-	break;
+		break;
 
 
 	case EVENT_ENUM::DAY_START:
@@ -117,9 +117,9 @@ void ModuleAI::ExecuteEvent(EVENT_ENUM eventId)
 int ModuleAI::CheckBaseAligmentAttack()
 {
 	int numBases = baseVector.size();
-	
 
-	for (int i = numBases-1; i >= 0; i--)
+
+	for (int i = numBases - 1; i >= 0; i--)
 	{
 		if (ENTITY_ALIGNEMENT::PLAYER == baseVector[i]->GetAlignment())
 		{
