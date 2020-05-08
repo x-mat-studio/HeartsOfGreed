b72912b4b051453fd4f738ecd6d617b977df6399
jose-tello <47975174+jose-tello@users.noreply.github.com>
2020-05-08 18:03:34 +0200

    0.8.29.3 Particle system files added from my personal research

--- Hearts Of Greed/Motor2D/ParticleSystem.h
+++ Hearts Of Greed/Motor2D/ParticleSystem.h
@@ -1,1 +1,38 @@
-#pragma once
+#ifndef __PARTICLE_SYSTEM_H__
+#define __PARTICLE_SYSTEM_H__
+
+#include "vector"
+#include "p2Point.h"
+
+class Emitter;
+
+class ParticleSystem
+{
+public:
+	ParticleSystem();
+	ParticleSystem(float x, float y);
+
+	~ParticleSystem();
+
+	void PushEmiter(Emitter& emitter);
+
+	void Update(float dt);
+	void PostUpdate(float dt);
+
+	void Desactivate();
+	void Activate();
+
+	bool IsActive();
+
+	void Move(int x, int y);
+
+private:
+	std::vector<Emitter> emitterVector;
+	fMPoint	position;
+
+	bool active;
+};
+
+
+
+#endif // __PARTICLE_SYSTEM_H__
