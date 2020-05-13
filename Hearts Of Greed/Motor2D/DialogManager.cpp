0437f6c7843838658b0b4d94f78915aaa993f65f
jose-tello <47975174+jose-tello@users.noreply.github.com>
2020-05-13 22:08:09 +0200

    0.8.14.0 Dialog manager finished

--- Hearts Of Greed/Motor2D/DialogManager.cpp
+++ Hearts Of Greed/Motor2D/DialogManager.cpp
@@ -4,8 +4,8 @@ ModuleDialogManager::ModuleDialogManager() :
 
 	Module(),
 
-	currentDialog1stCharacter(nullptr),
-	currentDialog2ndCharacter(nullptr),
+	currentDialog1(nullptr),
+	currentDialog2(nullptr),
 
 	//TUTORIAL-------------
 	dialogTutorialStart(),
@@ -35,10 +35,10 @@ ModuleDialogManager::ModuleDialogManager() :
 
 	//Quest 4 ------------
 	dialogMission4_ST_A1(),
-	dialogMission4_ST_B1(),
+	dialogMission4_ST_A2(),
 
-	dialogMission4_END_C1(),
-	dialogMission4_END_A2(),
+	dialogMission4_END_B1(),
+	dialogMission4_END_A3(),
 
 	input(DIALOG_INPUT::NULL_INPUT),
 	state(DIALOG_STATE::IDLE_DIALOG)
@@ -53,8 +53,8 @@ ModuleDialogManager::~ModuleDialogManager()
 	input = DIALOG_INPUT::INPUT_ERROR;
 	state = DIALOG_STATE::ERROR_DIALOG;
 
-	currentDialog1stCharacter = nullptr;
-	currentDialog2ndCharacter = nullptr;
+	currentDialog1 = nullptr;
+	currentDialog2 = nullptr;
 
 	//TUTORIAL-------------
 	 dialogTutorialStart.Clear();
@@ -84,10 +84,10 @@ ModuleDialogManager::~ModuleDialogManager()
 
 	//Quest 4 ------------
 	 dialogMission4_ST_A1.Clear();
-	 dialogMission4_ST_B1.Clear();
+	 dialogMission4_ST_A2.Clear();
 
-	 dialogMission4_END_C1.Clear();
-	 dialogMission4_END_A2.Clear();
+	 dialogMission4_END_B1.Clear();
+	 dialogMission4_END_A3.Clear();
 }
 
 
@@ -133,10 +133,10 @@ bool ModuleDialogManager::Awake(pugi::xml_node& data)
 
 	//Quest 4 ------------
 	 dialogMission4_ST_A1		= dialogue.child("dialog4").child("start").child("entry1").attribute("text").as_string();
-	 dialogMission4_ST_B1		= dialogue.child("dialog4").child("start").child("entry2").attribute("text").as_string();
+	 dialogMission4_ST_A2		= dialogue.child("dialog4").child("start").child("entry2").attribute("text").as_string();
 
-	 dialogMission4_END_C1		= dialogue.child("dialog4").child("finish").child("entry1").attribute("text").as_string();
-	 dialogMission4_END_A2		= dialogue.child("dialog4").child("finish").child("entry2").attribute("text").as_string();
+	 dialogMission4_END_B1		= dialogue.child("dialog4").child("finish").child("entry1").attribute("text").as_string();
+	 dialogMission4_END_A3		= dialogue.child("dialog4").child("finish").child("entry2").attribute("text").as_string();
 
 
 	diagDoc.reset();
@@ -155,13 +155,416 @@ bool ModuleDialogManager::PreUpdate(float dt)
 
 void ModuleDialogManager::InternalInput(float dt)
 {
-	// HERE GOES ALL TIMERS
+	// HERE GOES ALL TIMERS1_st_
 
 }
 
 
 void ModuleDialogManager::ProcessFsm()
 {
+	if (input != DIALOG_INPUT::NULL_INPUT)
+	{
+		switch (state)
+		{
+		case DIALOG_STATE::ERROR_DIALOG:
+			break;
+
+
+		case DIALOG_STATE::IDLE_DIALOG:
+			currentDialog1 = nullptr;
+			currentDialog2 = nullptr;
+
+			switch (input)
+			{
+			case DIALOG_INPUT::INPUT_ERROR:
+				assert("Input Error");
+				break;
+			
+
+			case DIALOG_INPUT::NEXT_DIALOG:
+				assert("Impossible to go to next dialog");
+				break;
+
+
+			case DIALOG_INPUT::TUTORIAL_START:
+				state = DIALOG_STATE::TUTORIAL_ST;
+
+				currentDialog1 = &dialogTutorialStart;
+				currentDialog2 = nullptr;
+				break;
+
+
+			case DIALOG_INPUT::TUTORIAL_END:
+				state = DIALOG_STATE::TUTORIAL_END;
+
+				currentDialog1 = &dialogTutorialEnd;
+				currentDialog2 = nullptr;
+				break;
+
+
+			case DIALOG_INPUT::MISSION_1_START:
+				state = DIALOG_STATE::MISSION1_ST_B1;
+
+				currentDialog1 = nullptr;
+				currentDialog2 = &dialogMission1_ST_B1;
+				break;
+
+
+			case DIALOG_INPUT::MISSION_1_END:
+				state = DIALOG_STATE::MISSION1_END_A2;
+
+				currentDialog1 = &dialogMission1_END_A2;
+				currentDialog2 = nullptr;
+				break;
+
+
+			case DIALOG_INPUT::MISSION_2_START:
+				state = DIALOG_STATE::MISSION2_ST_B1;
+
+				currentDialog1 = nullptr;
+				currentDialog2 = &dialogMission2_ST_B1;
+				break;
+
+
+			case DIALOG_INPUT::MISSION_2_END:
+				state = DIALOG_STATE::MISSION2_END_A2;
+
+				currentDialog1 = &dialogMission2_END_A2;
+				currentDialog2 = nullptr;
+				break;
+
+
+			case DIALOG_INPUT::MISSION_3_START:
+				state = DIALOG_STATE::MISSION3_ST_A1;
+
+				currentDialog1 = &dialogMission3_ST_A1;
+				currentDialog2 = nullptr;
+				break;
+
+
+			case DIALOG_INPUT::MISSION_3_END:
+				state = DIALOG_STATE::MISSION3_END_B1;
+
+				currentDialog1 = nullptr;
+				currentDialog2 = &dialogMission3_END_B1;
+				break;
+
+
+			case DIALOG_INPUT::MISSION_4_START:
+				state = DIALOG_STATE::MISSION4_ST_A1;
+
+				currentDialog1 = &dialogMission4_ST_A1;
+				currentDialog2 = nullptr;
+				break;
+
+
+			case DIALOG_INPUT::MISSION_4_END:
+				state = DIALOG_STATE::MISSION4_END_B1;
+
+				currentDialog1 = nullptr;
+				currentDialog2 = &dialogMission4_END_B1;
+				break;
+			}
+
+			input = DIALOG_INPUT::NULL_INPUT;
+			break;
+
+
+		case DIALOG_STATE::TUTORIAL_ST:
+			
+			switch (input)
+			{
+			case DIALOG_INPUT::NEXT_DIALOG:
+				state = DIALOG_STATE::IDLE_DIALOG;
+
+				currentDialog1 = nullptr;
+				currentDialog2 = nullptr;
+				break;
+			}
+
+			input = DIALOG_INPUT::NULL_INPUT;
+			break;
+
+
+		case DIALOG_STATE::TUTORIAL_END:
+
+			switch (input)
+			{
+			case DIALOG_INPUT::NEXT_DIALOG:
+				state = DIALOG_STATE::IDLE_DIALOG;
+
+				currentDialog1 = nullptr;
+				currentDialog2 = nullptr;
+				break;
+			}
+
+			input = DIALOG_INPUT::NULL_INPUT;
+			break;
+
+
+		case DIALOG_STATE::MISSION1_ST_B1:
+
+			switch (input)
+			{
+			case DIALOG_INPUT::NEXT_DIALOG:
+				state = DIALOG_STATE::MISSION1_ST_A1;
+
+				currentDialog1 = &dialogMission1_ST_A1;
+				break;
+			}
+
+			input = DIALOG_INPUT::NULL_INPUT;
+			break;
+
+
+		case DIALOG_STATE::MISSION1_ST_A1:
+
+			switch (input)
+			{
+			case DIALOG_INPUT::NEXT_DIALOG:
+				state = DIALOG_STATE::MISSION1_ST_B2;
+
+				currentDialog2 = &dialogMission1_ST_B2;
+				break;
+			}
+
+			input = DIALOG_INPUT::NULL_INPUT;
+			break;
+
+
+		case DIALOG_STATE::MISSION1_ST_B2:
+
+			switch (input)
+			{
+			case DIALOG_INPUT::NEXT_DIALOG:
+				state = DIALOG_STATE::IDLE_DIALOG;
+
+				currentDialog1 = nullptr;
+				currentDialog2 = nullptr;
+				break;
+			}
+
+			input = DIALOG_INPUT::NULL_INPUT;
+			break;
+
+
+		case DIALOG_STATE::MISSION1_END_A2:
+
+			switch (input)
+			{
+			case DIALOG_INPUT::NEXT_DIALOG:
+				state = DIALOG_STATE::MISSION1_END_B3;
+
+				currentDialog2 = &dialogMission1_END_B3;
+				break;
+			}
+
+			input = DIALOG_INPUT::NULL_INPUT;
+			break;
+
+
+		case DIALOG_STATE::MISSION1_END_B3:
+
+			switch (input)
+			{
+			case DIALOG_INPUT::NEXT_DIALOG:
+				state = DIALOG_STATE::IDLE_DIALOG;
+
+				currentDialog1 = nullptr;
+				currentDialog2 = nullptr;
+				break;
+			}
+
+			input = DIALOG_INPUT::NULL_INPUT;
+			break;
+
+
+		case DIALOG_STATE::MISSION2_ST_B1:
+
+			switch (input)
+			{
+			case DIALOG_INPUT::NEXT_DIALOG:
+				state = DIALOG_STATE::MISSION2_ST_A1;
+
+				currentDialog1 = &dialogMission2_ST_A1;
+				break;
+			}
+
+			input = DIALOG_INPUT::NULL_INPUT;
+			break;
+
+
+		case DIALOG_STATE::MISSION2_ST_A1:
+
+			switch (input)
+			{
+			case DIALOG_INPUT::NEXT_DIALOG:
+				state = DIALOG_STATE::IDLE_DIALOG;
+
+				currentDialog1 = nullptr;
+				currentDialog2 = nullptr;
+				break;
+			}
+
+			input = DIALOG_INPUT::NULL_INPUT;
+			break;
+
+
+		case DIALOG_STATE::MISSION2_END_A2:
+
+			switch (input)
+			{
+			case DIALOG_INPUT::NEXT_DIALOG:
+				state = DIALOG_STATE::MISSION2_END_B2;
+
+				currentDialog2 = &dialogMission2_END_B2;
+				break;
+			}
+
+			input = DIALOG_INPUT::NULL_INPUT;
+			break;
+
+
+		case DIALOG_STATE::MISSION2_END_B2:
+
+			switch (input)
+			{
+			case DIALOG_INPUT::NEXT_DIALOG:
+				state = DIALOG_STATE::IDLE_DIALOG;
+
+				currentDialog1 = nullptr;
+				currentDialog2 = nullptr;
+				break;
+			}
+
+			input = DIALOG_INPUT::NULL_INPUT;
+			break;
+
+
+		case DIALOG_STATE::MISSION3_ST_A1:
+			
+			switch (input)
+			{
+			case DIALOG_INPUT::NEXT_DIALOG:
+				state = DIALOG_STATE::IDLE_DIALOG;
+
+				currentDialog1 = nullptr;
+				currentDialog2 = nullptr;
+				break;
+			}
+
+			input = DIALOG_INPUT::NULL_INPUT;
+			break;
+
+
+		case DIALOG_STATE::MISSION3_END_B1:
+			
+			switch (input)
+			{
+			case DIALOG_INPUT::NEXT_DIALOG:
+				state = DIALOG_STATE::MISSION3_END_A2;
+
+				currentDialog1 = &dialogMission3_END_A2;
+				break;
+			}
+
+			input = DIALOG_INPUT::NULL_INPUT;
+			break;
+
+
+		case DIALOG_STATE::MISSION3_END_A2:
+
+			switch (input)
+			{
+			case DIALOG_INPUT::NEXT_DIALOG:
+				state = DIALOG_STATE::MISSION3_END_B2;
+
+				currentDialog2 = &dialogMission3_END_B2;
+				break;
+			}
+
+			input = DIALOG_INPUT::NULL_INPUT;
+			break;
+
+
+		case DIALOG_STATE::MISSION3_END_B2:
+
+			switch (input)
+			{
+			case DIALOG_INPUT::NEXT_DIALOG:
+				state = DIALOG_STATE::IDLE_DIALOG;
+
+				currentDialog1 = nullptr;
+				currentDialog2 = nullptr;
+				break;
+			}
+
+			input = DIALOG_INPUT::NULL_INPUT;
+			break;
+
+
+		case DIALOG_STATE::MISSION4_ST_A1:
+
+			switch (input)
+			{
+			case DIALOG_INPUT::NEXT_DIALOG:
+				state = DIALOG_STATE::MISSION4_ST_A2;
+
+				currentDialog1 = &dialogMission4_ST_A2;
+				break;
+			}
+
+			input = DIALOG_INPUT::NULL_INPUT;
+			break;
+
+
+		case DIALOG_STATE::MISSION4_ST_A2:
+
+			switch (input)
+			{
+			case DIALOG_INPUT::NEXT_DIALOG:
+				state = DIALOG_STATE::IDLE_DIALOG;
+
+				currentDialog1 = nullptr;
+				currentDialog2 = nullptr;
+				break;
+			}
+
+			input = DIALOG_INPUT::NULL_INPUT;
+			break;
+
+
+		case DIALOG_STATE::MISSION4_END_B1:
+
+			switch (input)
+			{
+			case DIALOG_INPUT::NEXT_DIALOG:
+				state = DIALOG_STATE::MISSION4_END_A3;
+
+				currentDialog1 = &dialogMission4_END_A3;
+				break;
+			}
+
+			input = DIALOG_INPUT::NULL_INPUT;
+			break;
+		case DIALOG_STATE::MISSION4_END_A3:
+
+			switch (input)
+			{
+			case DIALOG_INPUT::NEXT_DIALOG:
+				state = DIALOG_STATE::IDLE_DIALOG;
+
+				currentDialog1 = nullptr;
+				currentDialog2 = nullptr;
+				break;
+			}
+
+			input = DIALOG_INPUT::NULL_INPUT;
+			break;
+		default:
+			break;
+		}
+	}
 	
 }
 
@@ -182,11 +585,11 @@ bool ModuleDialogManager::PushInput(DIALOG_INPUT inp)
 
 P2SString* ModuleDialogManager::GetCurrentString1() const
 {
-	return currentDialog1stCharacter;
+	return currentDialog1;
 }
 
 
 P2SString* ModuleDialogManager::GetCurrentString2() const
 {
-	return currentDialog2ndCharacter;
+	return currentDialog2;
 }
