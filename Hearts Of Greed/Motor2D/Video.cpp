f4dfb9daefa7e0b935e88fc37a9d9b9a44d25ab3
Aaron Guerrero Cruz <47571598+AaronGCProg@users.noreply.github.com>
2020-05-17 17:48:34 +0200

    0.8.33.0 Video Module Setup

---
+++ Hearts Of Greed/Motor2D/Video.cpp
@@ -0,0 +1,115 @@
+#include "p2Log.h"
+#include "App.h"
+#include "Window.h"
+#include "Textures.h"
+#include "Render.h"
+#include "Audio.h"
+#include "Video.h"
+
+#include "SDL_mixer\include\SDL_mixer.h"
+
+Video::Video()
+{
+	name.create("video");
+}
+
+Video::~Video()
+{
+}
+
+bool Video::Update(float dt)
+{
+	if (!isVideoFinished)
+	{
+		GrabAVIFrame();
+	}
+	else
+		Mix_PauseMusic();
+	return true;
+}
+
+bool Video::Awake(pugi::xml_node&)
+{
+	return true;
+}
+
+bool Video::CleanUp()
+{
+	return true;
+}
+
+void Video::Initialize(char* path)
+{
+	OpenAVI(path);                  // Open The AVI File
+	app->audio->PlayMusic("video/sample.ogg", 0.0f);
+}
+
+void Video::OpenAVI(char* path)
+{
+	if (AVIStreamOpenFromFile(&pavi, path, streamtypeVIDEO, 0, OF_READ, NULL) != 0) // Opens The AVI Stream
+		LOG("Failed To Open The AVI Stream");
+
+
+	AVIStreamInfo(pavi, &psi, sizeof(psi));					// Reads Information About The Stream Into psi
+	width = psi.rcFrame.right - psi.rcFrame.left;           // Width Is Right Side Of Frame Minus Left
+	height = psi.rcFrame.bottom - psi.rcFrame.top;          // Height Is Bottom Of Frame Minus Top
+	lastFrame = AVIStreamLength(pavi);						// The Last Frame Of The Stream
+
+
+	pgf = AVIStreamGetFrameOpen(pavi, (LPBITMAPINFOHEADER)AVIGETFRAMEF_BESTDISPLAYFMT);              // Create The PGETFRAME Using Our Request Mode
+	if (pgf == NULL)
+		LOG("Failed To Open The AVI Frame");
+
+	isVideoFinished = false;
+}
+
+bool Video::GrabAVIFrame()
+{
+
+	LPBITMAPINFOHEADER lpbi;													// Holds The Bitmap Header Information
+	lpbi = (LPBITMAPINFOHEADER)AVIStreamGetFrame(pgf, frame);					// Grab Data From The AVI Stream
+	pdata = (char*)lpbi + lpbi->biSize + lpbi->biClrUsed * sizeof(RGBQUAD);    // Pointer To Data Returned By AVIStreamGetFrame
+																				// (Skip The Header Info To Get To The Data)
+																				// Convert Data To Requested Bitmap Format
+
+
+	SDL_Surface* surface = SDL_CreateRGBSurfaceFrom(pdata, width, height, lpbi->biBitCount, width * 3, 0, 0, 0, 0);
+	SDL_Texture* texture = app->tex->LoadSurface(surface);
+
+
+	app->render->Blit(texture, 0, 0, NULL);
+
+
+	if (i % 2 == 0)
+	{
+		frame++;
+	}
+	i++;
+	if (frame >= lastFrame)
+	{
+		frame = 0;
+		isVideoFinished = true;
+	}
+
+
+	app->tex->UnLoad(texture);
+	SDL_FreeSurface(surface);
+
+
+
+	char   title[100];
+	mpf = AVIStreamSampleToTime(pavi, lastFrame) / lastFrame;        // Calculate Rough Milliseconds Per Frame
+	wsprintf(title, "AVI Player: Width: %d, Height: %d, Frames: %d, Miliseconds per frame: %d", width, height, lastFrame, mpf);
+	app->win->SetTitle(title);
+
+	return true;
+}
+
+void Video::CloseAVI()
+{
+	AVIStreamGetFrameClose(pgf);                // Deallocates The GetFrame Resources
+
+	AVIStreamRelease(pavi);                     // Release The Stream
+
+	AVIFileExit();                              // Release The File
+}
---
+++ Hearts Of Greed/Motor2D/Video.h
@@ -0,0 +1,54 @@
+#ifndef __VIDEO_H__
+#define __VIDEO_H__
+
+#include "Module.h"
+
+//Video player for windows libraries
+#include <dshow.h>
+#include <Vfw.h>                          
+#pragma comment(lib,"vfw32.lib")               
+
+
+class Video : public Module
+{
+public:
+
+	Video();
+
+	// Destructor
+	virtual ~Video();
+	bool Update(float dt);
+
+	// Called before render is available
+	bool Awake(pugi::xml_node&);
+
+	// Called before quitting
+	bool CleanUp();
+
+	void Initialize(char* path);
+	void OpenAVI(char* path);
+	bool GrabAVIFrame();
+
+	void CloseAVI();
+
+public:
+	bool				isVideoFinished = false;
+
+private:
+	// Frame Counter
+	int			frame = 0;
+
+	AVISTREAMINFO       psi;                        // Pointer To A Structure Containing Stream Info
+	PAVISTREAM			pavi;                       // Handle To An Open Stream
+	PGETFRAME			pgf;                        // Pointer To A GetFrame Object
+
+	long				lastFrame;                  // Last Frame Of The Stream
+	int					width;                      // Video Width
+	int					height;                     // Video Height
+	char* pdata;					    // Pointer To Texture Data
+	int					mpf;                        // Will Hold Rough Milliseconds Per Frame
+
+	int i = 1;
+};
+
+#endif // __Video_H__
